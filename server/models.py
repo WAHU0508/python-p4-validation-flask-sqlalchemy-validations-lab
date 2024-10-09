1af8c748f97ccdf71f279d18c54fd9157be04c96
WAHU0508 <sharongikenye@gmail.com>
2024-10-09 08:29:07 +0300

    Add validations

--- server/models.py
+++ server/models.py
@@ -11,7 +11,24 @@ class Author(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators 
+    # Add validators
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError("Name cannot be empty")
+        
+        existing_author = Author.query.filter(Author.name == name).first()
+
+        if existing_author:
+            raise ValueError("Name must be unique")
+        return name
+    
+    @validates('phone_number')
+    def validate_phone_number(self, key, phone_number):
+        if len(phone_number) != 10 or not phone_number.isdigit():
+            raise ValueError("Phone number must be exactly 10 digits")
+        return phone_number
+    
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
@@ -26,9 +43,34 @@ class Post(db.Model):
     summary = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
-
+    
+    CLICKBAIT_PHRASES = ["Won't Believe", "Secret", "Top", "Guess"]
     # Add validators  
-
+    @validates('title')
+    def validate_title(self, key, title):
+        if not title:
+            raise ValueError("Title cannot be empty")
+        if not any(phrase in title for phrase in self.CLICKBAIT_PHRASES):
+            raise ValueError("Title must contail clickbait")
+        return title
+    
+    @validates('content')
+    def validate_content(self, key, content):
+        if len(content) < 250:
+            raise ValueError("Summary cannot exceed 250 characters")
+        return content
+    
+    @validates('summary')
+    def validate_summary(self, key, summary):
+        if summary and len(summary) > 250:
+            raise ValueError("Summary cannot exceed 250 characters")
+        return summary
+    
+    @validates('category')
+    def validate_category(self, key, category):
+        if category not in ['Fiction', 'Non-Fiction']:
+            raise ValueError("Category must be either 'Fiction' or 'Non-Fiction'")
+        return category
 
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
